725c4aa4222111f31e47c02d6f9319353f625116
torky <tylercai@Tylers-MacBook-Pro.local>
2013-12-07 17:37:02 -0500

    Conflict

--- src/tetris/Logic.java
+++ src/tetris/Logic.java
@@ -11,6 +11,22 @@ package tetris;
  * @author joelnpam
  */
 public class Logic {
-        int[][] array = new int[10][20];
+//<<<<<<< HEAD
 
+//=======
+    
+    int[][] positions = new int[10][20];
+    int[][] colots = new int [10][20];
+    
+    
+    public Logic() {
+        SquareBlock currentBlock = new SquareBlock();
+    }
+    
+    public void gameTick() {
+        
+     
+    }
+    
+//>>>>>>> 170513f5bad84d0de3511f16cea09f05cb0923f8
 }
